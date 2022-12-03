3e0e26906125caae36502a8cda850a70ec588752
Paweł Recław <pawel.reclaw@gmail.com>
2022-12-03 10:43:28 +0100

    Pierwszy commit

---
+++ src/main/java/pl/sda/j133/hiberanate/warsztat/Main.java
@@ -0,0 +1,12 @@
+package pl.sda.j133.hiberanate.warsztat;
+
+/**
+ * @author Paweł Recław, AmeN
+ * @project warsztat
+ * @created 03.12.2022
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
